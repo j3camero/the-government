01f1565e62be36d9797e90ca086595af997fc2d7
Jeff Cameron <cameron.jp@gmail.com>
2017-06-15 01:00:32 +0000

    Combined chatbot and webserver.

--- commissar.js
+++ commissar.js
@@ -1,4 +1,8 @@
 const Discord = require('discord.js');
+var express = require('express');
+var session = require('express-session');
+var passport = require('passport');
+var DiscordStrategy = require('passport-discord').Strategy;
 
 const token = '***REMOVED***';
 
@@ -98,3 +102,55 @@ client.on('guildMemberRemove', member => {
 });
 
 client.login(token);
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+var scopes = ['identify', 'email', /* 'connections', (it is currently broken) */ 'guilds', 'guilds.join'];
+
+passport.use(new DiscordStrategy({
+    clientID: '***REMOVED***',
+    clientSecret: '***REMOVED***',
+    callbackURL: 'http://secretclan.net:5000/callback',
+    scope: scopes
+}, function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function() {
+        return done(null, profile);
+    });
+}));
+
+var app = express();
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.get('/', passport.authenticate('discord', { scope: scopes }), function(req, res) {});
+app.get('/callback',
+    passport.authenticate('discord', { failureRedirect: '/' }), function(req, res) { res.redirect('/info') } // auth success
+);
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+app.get('/info', checkAuth, function(req, res) {
+    //console.log(req.user)
+    res.json(req.user);
+});
+
+function checkAuth(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.send('not logged in :(');
+}
+
+app.listen(5000, function (err) {
+    if (err) return console.log(err)
+    console.log('Listening at http://localhost:5000/')
+})
