0020b6ae7243341ea74540104d53b1524c3dca7d
Jeff Cameron <cameron.jp@gmail.com>
2020-03-04 07:52:11 +0000

    Fixed Mark name. Certain unicode chars are substituted now.

--- commissar.js
+++ commissar.js
@@ -79,11 +79,26 @@ function UpdateMemberRankRoles(member, rankName) {
 // Removes some characters, replaces others.
 function FilterUsername(username) {
     const allowedChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.-_` ()!?\'*+/\\:=~';
+    const substitutions = {
+	'ғ': 'f',
+	'ｕ': 'U',
+	'ᶜ': 'c',
+	'Ќ': 'K',
+	'ץ': 'Y',
+	'๏': 'o',
+	'Ữ': 'u',
+	'Ｍ': 'M',
+	'Ａ': 'A',
+	'ŕ': 'r',
+	'Ｋ': 'K',
+    };
     let s = '';
     for (let i = 0; i < username.length; i++) {
 	const c = username.charAt(i);
 	if (allowedChars.indexOf(c) >= 0) {
 	    s += c;
+	} else if (c in substitutions) {
+	    s += substitutions[c];
 	}
     }
     const maxNameLength = 18;
