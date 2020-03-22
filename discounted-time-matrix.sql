c4c06b18541dfed15ebaa92ef6d1d48647d4edb4
Jeff Cameron <cameron.jp@gmail.com>
2020-03-22 01:43:44 +0000

    Backfilled the time matrix database.

--- discounted-time-matrix.sql
+++ discounted-time-matrix.sql
@@ -1,4 +1,10 @@
 SELECT
-  duration_seconds,
-  EXP(0.0000000439290175 * TIMESTAMPDIFF(SECOND, NOW(), t)) * duration_seconds AS discounted
-FROM time_together;
+  u1.nickname,
+  u2.nickname,
+  SUM(EXP(0.0000000439290175 * TIMESTAMPDIFF(SECOND, NOW(), tt.t)) * tt.diluted_seconds) AS discounted_diluted_seconds
+FROM time_together AS tt
+INNER JOIN users AS u1 ON u1.commissar_id = tt.lo_user_id
+INNER JOIN users AS u2 ON u2.commissar_id = tt.hi_user_id
+GROUP BY tt.lo_user_id, tt.hi_user_id
+ORDER BY SUM(EXP(0.0000000439290175 * TIMESTAMPDIFF(SECOND, NOW(), tt.t)) * tt.diluted_seconds)
+;
