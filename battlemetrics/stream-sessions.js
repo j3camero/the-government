622eaa034447173739f4fea79e9d5c0464de23f5
Jeff Cameron <cameron.jp@gmail.com>
2020-09-14 06:18:57 +0000

    Dynamic rate limit added to crawler.

--- battlemetrics/stream-sessions.js
+++ battlemetrics/stream-sessions.js
@@ -2,7 +2,7 @@ const bearerToken = require('./bearer-token');
 const db = require('../database');
 const request = require('request');
 
-function FetchUrlAndFollowNextPage(url) {
+function FetchUrl(url) {
     const options = {
 	auth: {
 	    bearer: bearerToken,
@@ -13,28 +13,60 @@ function FetchUrlAndFollowNextPage(url) {
 	url,
     };
     console.log(`Fetching URL: ${url}`);
-    request.get(options, (error, response, body) => {
-	if (error) {
-	    console.error('error:', error);
-	    console.log('statusCode:', response && response.statusCode);
-	    return;
-	}
-	const parsed = JSON.parse(body);
-	const sessions = parsed.data;
-	console.log(`Parsed sessions: ${sessions.length}`);
-	console.log('Trying to insert the sessions into the database.');
-	db.writeBattlemetricsSessions(sessions);
-	if (parsed.links) {
-	    if (parsed.links.next) {
-		const nextPageUrl = parsed.links.next;
-		console.log(`Next page: ${nextPageUrl}`);
-		setTimeout(() => {
-		    console.log('Would get next page now, but stopping instead.');
-		    //FetchUrlAndFollowNextPage(nextPageUrl);
-		}, 1100);
-	    }
+    request.get(options, HandleSessionsResponse);
+}
+
+function HandleHttpErrors(error, response) {
+    if (error) {
+	console.error('error:', error);
+	console.log('HTTP', response ? response.statusCode : 'status code unknown.');
+	throw 'Error while fetching from Battlemetrics API.';
+    }
+}
+
+function HandleSessionsResponse(error, response, body) {
+    HandleHttpErrors(error, response);
+    const rateLimit = response.headers['x-rate-limit-limit'];
+    const remaining = response.headers['x-rate-limit-remaining'];
+    const parsed = JSON.parse(body);
+    const sessions = parsed.data;
+    db.writeBattlemetricsSessions(sessions);
+    EnforceRateLimit(rateLimit, remaining, () => {
+	if (parsed.links && parsed.links.next) {
+	    FetchUrl(parsed.links.next);
+	} else {
+	    StartNewCrawl();
 	}
     });
 }
 
-FetchUrlAndFollowNextPage('https://api.battlemetrics.com/sessions?filter%5Bgames%5D=rust&filter%5Bat%5D=2020-08-04T01%3A45%3A00Z&page%5Bsize%5D=100');
+// Implements a delay to respect the Battlemetrics API rate limit.
+// The callback is called after the delay.
+function EnforceRateLimit(rateLimit, remaining, callback) {
+    // The crawler won't crawl faster than this maximum rate limit per minute.
+    const maxRateLimit = 300;
+    // The minimum rate limit is a safe setting to use for backing off.
+    const minRateLimit = 5;
+    // Parse the number of requests remaining. It might be a string.
+    remaining = parseInt(remaining) || 0;
+    // Parse the rate limit. It might be a string.
+    rateLimit = parseInt(rateLimit);
+    if (!rateLimit || remaining < 1 || rateLimit < minRateLimit) {
+	rateLimit = minRateLimit;
+    }
+    if (rateLimit > maxRateLimit) {
+	rateLimit = maxRateLimit;
+    }
+    // Undershoot the rate limit to be courteous.
+    if (rateLimit > minRateLimit) {
+	rateLimit -= 3;
+    }
+    const delayMs = 60 * 1000 / rateLimit;
+    setTimeout(callback, delayMs);
+}
+
+function StartNewCrawl() {
+    console.log('Start a new crawl.');
+}
+
+FetchUrl('https://api.battlemetrics.com/sessions?filter%5Bgames%5D=rust&filter%5Bat%5D=2020-08-04T01%3A45%3A00Z&page%5Bsize%5D=100');
--- database.js
+++ database.js
@@ -144,12 +144,15 @@ function writeBattlemetricsSessions(sessions) {
 	'    battlemetrics_sessions.in_game_name = VALUES(battlemetrics_sessions.in_game_name), ' +
 	'    battlemetrics_sessions.server_id = VALUES(battlemetrics_sessions.server_id), ' +
 	'    battlemetrics_sessions.player_id = VALUES(battlemetrics_sessions.player_id), ' +
-	'    battlemetrics_sessions.identifier_id = VALUES(battlemetrics_sessions.identifier_id)');
+	    '    battlemetrics_sessions.identifier_id = VALUES(battlemetrics_sessions.identifier_id)');
+    // Time the database operation.
+    const startTime = Date.now();
     connection.query(sql, (err, result) => {
 	if (err) {
 	    throw err;
 	}
-	console.log(`Wrote ${sessions.length} Battlemetrics sessions.`);
+	const elapsed = Date.now() - startTime;
+	console.log(`Wrote ${sessions.length} Battlemetrics sessions to the DB. [${elapsed} ms]`);
     });
 }
 
