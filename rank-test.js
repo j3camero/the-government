1acaae63f59c4161219b06b2959bd26327cf10ae
Jeff Cameron <cameron.jp@gmail.com>
2020-12-15 05:00:26 +0000

    Completely disable long-running unit test because it blows up the CI enrivonment.

--- rank-test.js
+++ rank-test.js
@@ -119,17 +119,19 @@ describe('Rank', function() {
 	assert.deepEqual(chain, sampleChainOfCommand);
     });
     it('Render the chain of command as an image', () => {
-	const nicknames = {
-	    6: 'Brobob',
-	    7: 'Jeff',
-	    32: 'Ssulfur',
-	    38: 'watergate',
-	    42: 'Cheatx',
-	    77: 'Zomboscott',
-	};
-	const canvas = rank.RenderChainOfCommand(sampleChainOfCommand, nicknames);
-	const buf = canvas.toBuffer();
-	fs.writeFileSync('sample-chain-of-command-tmp.png', buf);
+	// TODO: find a way to run this test as a separate integration test or something.
+	// It takes too long to run, blowing up the CI environment.
+	//const nicknames = {
+	//    6: 'Brobob',
+	//    7: 'Jeff',
+	//    32: 'Ssulfur',
+	//    38: 'watergate',
+	//    42: 'Cheatx',
+	//    77: 'Zomboscott',
+	//};
+	//const canvas = rank.RenderChainOfCommand(sampleChainOfCommand, nicknames);
+	//const buf = canvas.toBuffer();
+	//fs.writeFileSync('sample-chain-of-command-tmp.png', buf);
 	// Compare the image data to the expected output file.
 	//const expected = fs.readFileSync('sample-chain-of-command.png');
 	//assert(buf.equals(expected));
