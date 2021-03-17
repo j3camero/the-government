b1d6addec0fd3ac6cd99044af1d02c6e45458a0b
Jeff Cameron <cameron.jp@gmail.com>
2021-03-17 05:18:23 +0000

    Added some executive offices/titles for more of the ranks.

--- executive-config.js
+++ executive-config.js
@@ -1,11 +1,11 @@
 module.exports = {
     'PRES': {
-	rank: 0,
 	title: 'President',
+	rank: 0,
     },
     'VP': {
-	rank: 1,
 	title: 'Vice President',
+	rank: 1,
     },
     'CJCS': {
 	personalRole: 'Chairman of the Joint Chiefs of Staff',
@@ -26,7 +26,7 @@ module.exports = {
 	recursiveRole: 'Navy',
     },
     'AIR': {
-	personalRole: 'Commander of the Air Force',
+	personalRole: 'Administrator of the Air Force',
 	rank: 3,
 	recursiveRole: 'Air Force',
     },
@@ -34,5 +34,49 @@ module.exports = {
 	personalRole: 'Commandant of the Marines',
 	rank: 3,
 	recursiveRole: 'Marines',
+    },
+    'INTER': {
+	personalRole: 'Minister of the Interior',
+	rank: 4,
+    },
+    'FOR': {
+	personalRole: 'Minister of Foreign Affairs',
+	rank: 4,
+    },
+    'JUS': {
+	personalRole: 'Minister of Justice',
+	rank: 4,
+    },
+    'INTEL': {
+	personalRole: 'Director of Intelligence',
+	rank: 4,
+    },
+    'FIN': {
+	personalRole: 'Minister of Finance',
+	rank: 4,
+    },
+    'DEPINTER': {
+	personalRole: 'Deputy Minister of the Interior',
+	rank: 5,
+    },
+    'DEPFOR': {
+	personalRole: 'Deputy Minister of Foreign Affairs',
+	rank: 5,
+    },
+    'DEPJUS': {
+	personalRole: 'Deputy Minister of Justice',
+	rank: 5,
+    },
+    'DEPINTEL': {
+	personalRole: 'Deputy Director of Intelligence',
+	rank: 5,
+    },
+    'DEPFIN': {
+	personalRole: 'Deputy Minister of Finance',
+	rank: 5,
+    },
+    'OPS': {
+	personalRole: 'Director of Special Ops',
+	rank: 5,
     },
 };
