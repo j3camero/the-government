85b421ea829394cc575a1f345851322291ae9bba
Jeff Cameron <cameron.jp@gmail.com>
2022-05-20 22:21:34 +0000

    Added room sort order.

--- huddles.js
+++ huddles.js
@@ -45,7 +45,6 @@ async function CreateNewVoiceChannelWithBitrate(guild, huddle, bitrate) {
 	    { id: RoleID.Grunt, allow: perms },
 	    { id: RoleID.Bots, allow: perms },
 	],
-	position: huddle.position,
 	type: 'voice',
 	userLimit: huddle.userLimit,
     };
@@ -107,22 +106,111 @@ async function UpdateVoiceChannelsForOneHuddleType(guild, huddle) {
     }
 }
 
+// A comparator for Discord rooms. Controls the sort order of the rooms.
+function CompareRooms(a, b) {
+    // Never sort rooms that have a parent.
+    if (a.parent || b.parent) {
+	return 0;
+    }
+    // Empty rooms sort down.
+    if (a.members.size === 0 && b.members.size > 0) {
+	return 1;
+    }
+    if (a.members.size > 0 && b.members.size === 0) {
+	return -1;
+    }
+    // Full rooms sort down.
+    if (a.full && !b.full) {
+	return 1;
+    }
+    if (!a.full && b.full) {
+	return -1;
+    }
+    // Rooms named Main sort up.
+    if (a.name !== 'Main' && b.name === 'Main') {
+	return 1;
+    }
+    if (a.name === 'Main' && b.name !== 'Main') {
+	return -1;
+    }
+    // Rooms named Officers Only sort down.
+    if (a.name === 'Officers Only' && b.name !== 'Officers Only') {
+	return 1;
+    }
+    if (a.name !== 'Officers Only' && b.name === 'Officers Only') {
+	return -1;
+    }
+    // Rooms with more people in them sort down.
+    if (a.members.size > b.members.size) {
+	return 1;
+    }
+    if (a.members.size < b.members.size) {
+	return -1;
+    }
+    // Rooms with higher capacity sort down.
+    if (a.userLimit > b.userLimit) {
+	return 1;
+    }
+    if (a.userLimit < b.userLimit) {
+	return -1;
+    }
+    return 0;
+}
+
+// Checks the see if any rooms need to be moved in the ordering, and does so.
+// Only moves one room at a time. To achieve a full sort, call this periodically.
+// Returns true if no rooms needed moving. Returns false if a room was moved.
+async function MoveOneRoomIfNeeded(guild) {
+    const rooms = [];
+    for (const [id, channel] of guild.channels.cache) {
+	if (channel.type === 'voice' && !channel.parent) {
+	    rooms.push(channel);
+	}
+    }
+    console.log('Sorting', rooms.length, 'voice rooms.');
+    // Sort the rooms internally by their position in Discord.
+    rooms.sort((a, b) => {
+	if (a.position < b.position) {
+	    return -1;
+	}
+	if (a.position > b.position) {
+	    return 1;
+	}
+	return 0;
+    });
+    for (const room of rooms) {
+        console.log(room.name, room.position, room.members.size);
+    }
+    for (let i = 0; i < rooms.length - 1; i++) {
+	const a = rooms[i];
+	const b = rooms[i + 1];
+	if (CompareRooms(a, b) > 0) {
+	    console.log('Swapping two voice rooms', a.name, 'and', b.name);
+	    await a.setPosition(1, { relative: true });
+	    return false;
+	}
+    }
+    console.log('No rooms sorted.');
+    return true;
+}
+
 // To avoid race conditions on the cheap, use a system of routine updates.
 // To schedule an update, a boolean flag is flipped. That way, the next time
 // the cycle goes around, it knows that an update is needed. Redundant or
 // overlapping updates are avoided this way.
 let isUpdateNeeded = false;
-setInterval(Update, 10 * 1000);
+setInterval(Update, 5 * 1000);
 
 async function Update() {
     if (!isUpdateNeeded) {
 	return;
     }
-    isUpdateNeeded = false;
     const guild = await DiscordUtil.GetMainDiscordGuild();
     for (const huddle of huddles) {
 	await UpdateVoiceChannelsForOneHuddleType(guild, huddle);
     }
+    const roomsInOrder = await MoveOneRoomIfNeeded(guild);
+    isUpdateNeeded = !roomsInOrder;
 }
 
 function ScheduleUpdate() {
