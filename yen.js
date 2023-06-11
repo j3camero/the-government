0625d63d6cd287c322f3cd3cbac6fef561e16d32
Ubuntu <ubuntu@ip-172-31-40-168.us-west-2.compute.internal>
2023-06-11 20:49:08 +0000

    Update yen reserve amount.

--- yen.js
+++ yen.js
@@ -346,7 +346,7 @@ async function UpdateYenChannel() {
     const channel = await guild.channels.resolve(yenChannelId);
     await channel.bulkDelete(99);
     await DiscordUtil.SendLongList(lines, channel);
-    const jeffSteamInventoryValue = 92745;
+    const jeffSteamInventoryValue = 109925;
     const reserveRatio = jeffSteamInventoryValue / totalYen;
     const formattedReserveRatio = parseInt(reserveRatio * 100);
     let message = '';
