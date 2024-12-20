5a50d05fd9dc10fe82eb00f026605f5fec259b7b
Ubuntu <ubuntu@ip-172-31-40-168.us-west-2.compute.internal>
2024-12-20 17:26:13 +0000

    Slow down HC decay a bit

--- discounted-time-matrix-16h.sql
+++ discounted-time-matrix-16h.sql
@@ -1,12 +1,12 @@
 SELECT
   tt.lo_user_id AS lo_user_id,
   tt.hi_user_id AS hi_user_id,
-  SUM(EXP(0.000000267417893734547 * TIMESTAMPDIFF(SECOND, NOW(), tt.t)) * tt.diluted_seconds) AS discounted_diluted_seconds
+  SUM(EXP(0.00000008913929791 * TIMESTAMPDIFF(SECOND, NOW(), tt.t)) * tt.diluted_seconds) AS discounted_diluted_seconds
 FROM time_together AS tt
 INNER JOIN users AS lo on lo.commissar_id = tt.lo_user_id
 INNER JOIN users AS hi ON hi.commissar_id = tt.hi_user_id
 WHERE lo.citizen IS TRUE AND hi.citizen IS TRUE
 AND tt.t > NOW() - INTERVAL 16 HOUR
 GROUP BY tt.lo_user_id, tt.hi_user_id
-ORDER BY SUM(EXP(0.000000267417893734547 * TIMESTAMPDIFF(SECOND, NOW(), tt.t)) * tt.diluted_seconds)
+ORDER BY SUM(EXP(0.00000008913929791 * TIMESTAMPDIFF(SECOND, NOW(), tt.t)) * tt.diluted_seconds)
 ;
